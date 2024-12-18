c677c590d8bfbc97814667bed8b3ce8e64aad8b2
Arlanmt <157086266+Arlanmt@users.noreply.github.com>
2024-12-19 00:01:16 +0500

    Update PhDStudent.java

--- ResearchUni/src/users/PhDStudent.java
+++ ResearchUni/src/users/PhDStudent.java
@@ -1,8 +1,8 @@
-// Class: PhDStudent
 package users;
 
 import java.util.List;
-import researches.*;
+import researches.ResearchPaper;
+
 public class PhDStudent {
     private String phdThesisTopic;
     private Researcher supervisor;
@@ -27,8 +27,9 @@ public class PhDStudent {
     public int calculateHIndex() {
         return hIndex;
     }
-}
- // New Methods
+
+    // New Methods
+
     public void addPublishedPaper(ResearchPaper paper) {
         publishedPapers.add(paper);
     }
@@ -39,5 +40,39 @@ public class PhDStudent {
 
     public void incrementHIndex() {
         hIndex++;
+    }
+
+    /**
+     * Lists all published papers of the PhD student.
+     */
+    public void listPublishedPapers() {
+        if (publishedPapers.isEmpty()) {
+            System.out.println("No published papers.");
+        } else {
+            System.out.println("Published papers:");
+            for (ResearchPaper paper : publishedPapers) {
+                System.out.println("- " + paper.getTitle());
+            }
+        }
+    }
+
+    /**
+     * Checks if the PhD student is eligible for defending their thesis.
+     * A student is eligible if they have at least 3 published papers and an H-index of 5 or more.
+     *
+     * @return true if eligible, false otherwise.
+     */
+    public boolean isEligibleForDefense() {
+        return publishedPapers.size() >= 3 && hIndex >= 5;
+    }
+
+    /**
+     * Calculates the research impact of the PhD student.
+     * The impact is defined as the product of H-index and the number of published papers.
+     *
+     * @return research impact value.
+     */
+    public double calculateResearchImpact() {
+        return hIndex * publishedPapers.size();
     }
 }
